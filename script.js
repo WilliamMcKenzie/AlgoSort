b16f66cc196c937434cace4d7495cde0532a4348
WilliamMcKenzie <williamqmckenzie@gmail.com>
2023-09-16 16:02:02 -0300

    selection sort

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var elements = document.querySelectorAll(".sorting-content")
 
 var arr = []
+var numsArr = []
+
 var parent = document.getElementById("sorting-container")
 
 for (var i = 0; i < elements.length; i++) {
@@ -9,6 +11,7 @@ for (var i = 0; i < elements.length; i++) {
 
     elements[i].style = `height: ${(ranHeight * 20) + 90}px`
     arr.push(elements[i])
+    numsArr.push((ranHeight * 20) + 90)
 }
 
 function arrayMove(arr, old_index, new_index) {
@@ -16,12 +19,28 @@ function arrayMove(arr, old_index, new_index) {
     return arr;
 };
 
+function arrayPosition(arr, old_index, new_index){
+
+    if(old_index > new_index) {
+        var temp = old_index
+
+        old_index = new_index
+        new_index = temp
+    }
+
+    var newIndexVal = arr[new_index]
+
+    arr.splice(new_index-1, 1, arr.splice(old_index, 1)[0]);
+    arr.splice(old_index, 0, newIndexVal)
+    return arr
+}
+
 function elementMove(arr) {
+    elements = document.querySelectorAll(".sorting-content")
+    console.log(arr)
     while (parent.hasChildNodes()) parent.removeChild(parent.firstChild)
 
     for (var i = 0; i < arr.length; i++) {
-
-
         parent.appendChild(arr[i])
     }
 }
@@ -29,7 +48,7 @@ function elementMove(arr) {
 //sorting functions
 
 async function bubbleSort(arr) {
-    var delay = 1000
+    var delay = 500
 
     for (var i = 0; i < arr.length; i++) {
         setTimeout(function () {
@@ -44,41 +63,59 @@ async function bubbleSort(arr) {
             }
 
         }, delay);
-        delay += 1000
+        delay += 500
     }
 
     return arr
 }
 
 async function selectionSort(arr) {
-    var delay = 1000
+    var delay = 200
     var minPos = 0
-    var sortedBorder = 0
-
+    
     for (var i = 0; i < arr.length; i++) {
-        setTimeout(function () {
+        minPos = i
+        console.log(arr)
 
-            for (var k = sortedBorder; k < arr.length; k++) {
+        var pauseCall = function (j) {
+            setTimeout(function() { 
+                console.log(i)
+            for (var k = i + 1; k < arr.length; k++) {
+
+                console.log(arr[minPos].offsetHeight, i)
+
                 if (arr[minPos].offsetHeight >= arr[k].offsetHeight) {
                     minPos = k
                 } else {
                     continue;
                 }
             }
-
-            arr = arrayMove(arr, sortedBorder, minPos)
             console.log(arr)
+            arr = arrayPosition(arr, i, minPos)
+            elementMove(arr)
+            }, delay);
+        }
 
-        }, delay);
+        pauseCall(i)
 
-        sortedBorder++
-        delay += 1000
+        delay += 200
     }
+    
+    var k = 0
 
+    if (arr[0].offsetHeight > arr[1].offsetHeight){
+        console.log("WOWWWW", arr[0], arr[1])
+        for(var i = 1; i < arr.length; i++) {
+            if(arr[k].offsetHeight >= arr[i].offsetHeight){
+                arrayMove(arr, k, i)
+                elementMove(arr)
+                k++
+            }
+        }
+    }
     return arr
 }
-
-console.log(bubbleSort(arr))
+console.log(selectionSort(arr))
 
 
 
@@ -102,3 +139,34 @@ function bubbleSortArray(arr) {
 
     return arr
 }
+
+async function selectionSortArray(arr) {
+        var delay = 1000
+        var minPos = 0
+    
+        for (var i = 0; i < arr.length; i++) {
+            minPos = i
+    
+            for (var k = i + 1; k < arr.length; k++) {
+    
+                console.log(arr)
+                console.log(minPos)
+
+                if (arr[minPos] >= arr[k]) {
+                    minPos = k
+                } else {
+                    continue;
+                }
+            }
+
+            if(minPos == i){
+
+            } else {
+                arr = arrayPosition(arr, i, minPos)
+            }
+    
+            delay += 1000
+        }
+    
+        return arr
+    }
